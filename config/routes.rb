db8ee3384df111e22f97edeef8d3d7f4c770d01a
gregory hanffou <ghanffou@gmail.com>
2018-08-22 00:46:12 +0200

    new rails app vila 85

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  get 'about',          to: 'restaurants#about',      as: :about
+  get 'contact',        to: 'restaurants#contact',    as: :contact
+  get 'direction',      to: 'restaurants#direction',  as: :direction
+  get 'restaurants',     to: 'restaurants#index',     as: :index
+  get 'restaurants/:id', to: 'restaurants#show',      as: :restaurant
+
+  resources :restaurants
+
 end
